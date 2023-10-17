a06081284cf614d8c22b7812cd7cd373c997f9db
26june <gj.abordo1@gmail.com>
2023-10-17 14:57:22 +0100

    add home styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import initialData from "./components/inital-data";
 import Home from "./components/Home";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: #424549;
+  height: 100vh;
+  width: 100vw;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <Container className="App">
       <Home></Home>
-    </div>
+    </Container>
   );
 }
 
