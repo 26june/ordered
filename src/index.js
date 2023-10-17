1868b75141a3c46e1d45f451dce150fc1420d5da
26june <gj.abordo1@gmail.com>
2023-10-17 14:57:15 +0100

    add reset

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "reset-css";
 import App from "./App";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+`;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <>
+    <GlobalStyle></GlobalStyle>
+    <App />
+  </>
+);
